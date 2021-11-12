3722b631f588a34d1cbef20b067ec54e4e650dec
Murillo <tumusx1@gmail.com>
2021-11-12 18:15:57 -0300

    edit files and structs

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const http = require ('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '127.0.0.1'; //enderecamento ip comecando com 100 
+const port = 3000; // defububdi a porta 
 
 const server = http.createServer((req,res)=>{
     res.statusCode=200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('hwlloqa')
+    res.end('Seja bem vindo a esse mundo maluco') // --> mensagem exibida no localhost 
 } );
 
 server.listen(port, hostname, ()=>{
-    console.log("Ta rodando ok");
+    console.log("Ta rodando ok"); // --> aqui retorna no terminal
 })
