57622c84da1490763fc97fc6d688897b9cf246bb
Murillo <tumusx1@gmail.com>
2021-11-12 23:45:40 -0300

    edit file

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const http = require ('http');
 
 const hostname = '127.0.0.1'; //enderecamento ip comecando com 100 
-const port = 3000; // defububdi a porta 
+const port = 3000; // definindo o número da porta 
 
 const server = http.createServer((req,res)=>{
     res.statusCode=200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Seja bem vindo a esse mundo maluco') // --> mensagem exibida no localhost 
+    res.end('Essa aplicação está funcioando') // --> mensagem exibida no localhost 
+    
 } );
 
 server.listen(port, hostname, ()=>{
